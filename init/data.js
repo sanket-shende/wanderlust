f517ac0f29e16f4b19c4d78dbdafe0732d02a407
sanket shende <sanketshende81@gmail.com>
2024-07-30 18:11:45 +0530

    Add Project Files

---
+++ init/data.js
@@ -0,0 +1,70 @@
+const sampleListings = [
+  {
+    title: "my dog",
+    description: "he good dog",
+    image: {
+      filename: "listingfilename1",
+      url: "https://images.pexels.com/photos/186077/pexels-photo-186077.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    price: 1200,
+    location: "chandrapur",
+    country: "india",
+  },
+  {
+    title: "new home",
+    description: "my sweet home",
+    image: {
+      filename: "listingfilename2",
+      url: "https://images.pexels.com/photos/1732414/pexels-photo-1732414.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    price: 20000,
+    location: "mumbai",
+    country: "india",
+  },
+  {
+    title: "good book",
+    description: "try this book",
+    image: {
+      filename: "listingfilename3",
+      url: "https://images.pexels.com/photos/3935320/pexels-photo-3935320.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    },
+    price: 1200,
+    location: "delhi",
+    country: "india",
+  },
+  {
+    title: "its me",
+    description: "good song",
+    image: {
+      filename: "listingfilename4",
+      url: "https://images.pexels.com/photos/1396132/pexels-photo-1396132.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    price: 1200,
+    location: "chicago",
+    country: "usa",
+  },
+  {
+    title: "ok yeeeessss",
+    description: "greate movie",
+    image: {
+      filename: "listingfilename5",
+      url: "https://images.pexels.com/photos/3958961/pexels-photo-3958961.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    },
+    price: 1200,
+    location: "lose angeles",
+    country: "usa",
+  },
+  {
+    title: "best",
+    description: "tooooooo",
+    image: {
+      filename: "listingfilename6",
+      url: "https://images.pexels.com/photos/3958961/pexels-photo-3958961.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    },
+    price: 120560,
+    location: "goooooo",
+    country: "meeeee",
+  },
+];
+
+module.exports = { data: sampleListings };
